fd7b228ac5b2769e07ab790381cacf57222b8216
Jotap53 <martinsjp53@gmail.com>
2026-03-27 18:54:36 +0000

    subindo novos arquivo do curso

---
+++ conteudo_do_curso/aula001.py
@@ -0,0 +1,12 @@
+'''Docstring
+E escrever o que eu
+quiser
+
+
+oskwoskwokswoksw
+'''
+
+# Permite escrever um comentário
+print(123) # Na frante
+# Abaixo
+print(456)
---
+++ conteudo_do_curso/aula002.py
@@ -0,0 +1,6 @@
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, 1011, sep='-', end='\n##') #sep será o separador que será usado entre os valores dentro do print.
+print(56, 78, sep='-', end='\n')
+print(9, 10, sep='-', end='\n')
+Print()
