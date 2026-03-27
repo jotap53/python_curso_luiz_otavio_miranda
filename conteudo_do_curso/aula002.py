0e3b0c2cd426271c9d0a1b76e1dd61a8aa11c341
Jotap53 <martinsjp53@gmail.com>
2026-03-27 19:26:38 +0000

    subindo novos arquivos do curso

--- conteudo_do_curso/aula002.py
+++ conteudo_do_curso/aula002.py
@@ -3,4 +3,4 @@
 print(12, 34, 1011, sep='-', end='\n##') #sep será o separador que será usado entre os valores dentro do print.
 print(56, 78, sep='-', end='\n')
 print(9, 10, sep='-', end='\n')
-Print()
+# Print()
